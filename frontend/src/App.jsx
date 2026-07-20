37d7636ed765c146c6919015c70eeb0f2a53f54c
Sanskruti Chikate <sanskrutichikate6@gmail.com>
2026-07-21 01:12:59 +0530

    Day 1 Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Insurance Management Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
